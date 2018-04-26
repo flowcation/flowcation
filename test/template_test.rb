db23eb57a9ba1607af8540a0471b9780c3eeebe9
Matthias Hennemeyer <mhennemeyer@gmail.com>
2018-04-26 11:51:41 +0200

    0.3.2

---
+++ test/template_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+require 'flowcation'
+
+class TemplateTest < Minitest::Test
+  
+   
+  def setup
+    @html = %q(
+<html>
+  <head>
+  </head>
+  <body>
+    <div>content</div>
+    <div class="attribute"></div>
+    <img class="klazz" src="img/image.jpg"></img>
+    <img class="klazz klazz-2" data-action="click->controller#action" src="img/image.jpg">
+  </body>
+</html>
+    )
+    @doc = Nokogiri::HTML(@html)
+  end
+  
+  def test_template
+    template = Flowcation::Template.new \
+      Nokogiri::HTML(@html).xpath("//body"), 
+      nil, 
+      'path', 
+      nil, 
+      [],
+      nil
+
+    assert_equal @html, template.content
+  end
+end
