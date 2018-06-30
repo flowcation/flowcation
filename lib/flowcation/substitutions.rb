36f2af7529da8dab121f813f774c54cc59ec8578
Matthias Hennemeyer <mhennemeyer@gmail.com>
2018-06-30 10:33:35 +0200

    0.3.9

--- lib/flowcation/substitutions.rb
+++ lib/flowcation/substitutions.rb
@@ -2,15 +2,17 @@ module Flowcation
   module Substitutions
     def substitute(doc)
       @substitutions&.each do |name, settings|
-        substitution = Substitution.new \
-          name, 
-          settings['xpath'], 
-          settings['substitute'], 
-          settings['value'], 
-          settings['key'],
-          settings['helper']
+        if settings # might be disabled by defining empty substitution, what results in nil settings 
+          substitution = Substitution.new \
+            name, 
+            settings['xpath'], 
+            settings['substitute'], 
+            settings['value'], 
+            settings['key'],
+            settings['helper']
           
-        substitution.apply(doc)
+          substitution.apply(doc)
+        end
       end
     end
   end
